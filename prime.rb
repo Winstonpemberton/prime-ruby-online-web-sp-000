9aea9ee3f1b186628c8ac067c2d60c502cb000e9
Winston Pemberton <winstonpemberton@gmail.com>
2019-11-12 00:10:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(interger)
-  if interger.negative? == true
+  if interger.negative? == true || interger == 0
     false
   else
     (2..interger - 1).none? {|i| interger % i == 0}
